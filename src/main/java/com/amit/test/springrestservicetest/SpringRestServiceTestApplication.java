2a962a153577d4a7e45d2a7241a11f596f85861b
Amit Verma <akvhbti606@gmail.com>
2022-04-10 15:36:22 +0530

    first init commit

---
+++ src/main/java/com/amit/test/springrestservicetest/SpringRestServiceTestApplication.java
@@ -0,0 +1,13 @@
+package com.amit.test.springrestservicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestServiceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestServiceTestApplication.class, args);
+	}
+
+}
