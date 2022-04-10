049b6c9a4c834e2dcf83e51a6fc019e488da8aaa
Amit Verma <akvhbti606@gmail.com>
2022-04-10 16:33:13 +0530

    my second commit

---
+++ src/main/java/com/amit/test/springrestservicetest/TestController.java
@@ -0,0 +1,13 @@
+package com.amit.test.springrestservicetest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @GetMapping
+    public String show() {
+        return "Hello from Test Service";
+    }
+}
